101ee541379b98cb28ade76ecfd45d19fb776828
Joselst0108 <joselst0108@gmail.com>
2026-03-02 02:05:38 -0500

    Actualizar guard.js

--- assets/js/guard.js
+++ assets/js/guard.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  await getUserProfile();
+  const p = await getUserProfile();
+  if (!p) return;
+
+  // 🔥 avisa al UI que ya hay contexto cargado
+  window.dispatchEvent(new Event("contextUpdated"));
 });
